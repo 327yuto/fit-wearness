9b6ed5717047f2ff1d638798b00e04c50c7ea975
327yuto <78023458+327yuto@users.noreply.github.com>
2022-07-29 16:13:00 +0900

    backend API設定

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+        include DeviseTokenAuth::Concerns::SetUserByToken
 
-  def good
-    render html: "good!API!" 
-  end
+  # def good
+  #   render html: "good!API!" 
+  # end
+
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :user_signed_in?
 
 end
---
+++ frontend/src/App.jsx
@@ -0,0 +1,78 @@
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { getCurrentUser } from "./api/auth";
+import { Home } from "./components/Home";
+import { SignIn } from "./components/SignIn";
+import { SignUp } from "./components/SignUp";
+
+export const AuthContext = createContext();
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser();
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.data);
+        console.log(res?.data.data);
+        console.log("sign in user")
+      } else {
+        console.log("no current user");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleGetCurrentUser();
+  }, [setCurrentUser]);
+
+  const Private = ({ children }) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children;
+      } else {
+        return <Redirect to="signin" />;
+      }
+    } else {
+      return <></>;
+    }
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        loading,
+        setLoading,
+        isSignedIn,
+        setIsSignedIn,
+        currentUser,
+        setCurrentUser,
+      }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Private>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Private>
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;

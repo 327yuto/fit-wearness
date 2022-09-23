6e16fa8c61f3f3eb7811d649306eed67ec2a9fe3
327yuto <78023458+327yuto@users.noreply.github.com>
2022-09-23 19:24:42 +0900

    clientのbaseURLを本番URLへ変更

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -11,7 +11,8 @@ const options = {
 }
 
 const client = applyCaseMiddleware(axios.create({
-  baseURL: "http://localhost:3020/api/v1"
+  // baseURL: "http://localhost:3020/api/v1"
+  baseURL: "https://fit-wearness-front.herokuapp.com/api/v1"
 }), options)
 
 export default client

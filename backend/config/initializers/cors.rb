92d299799eda47abc9ab9c384024c6126908859a
327yuto <78023458+327yuto@users.noreply.github.com>
2022-09-23 19:04:11 +0900

    corsへ本番URLの追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3002" #react側のポート番号
+    origins "localhost:3002" || "https://fit-wearness-front.herokuapp.com/"
+    # ReactのポートとヘロクのURL
 
     resource '*',
       headers: :any,

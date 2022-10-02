3aec9d67e04608c2944d9eb2fe34c1919f25c949
327yuto <78023458+327yuto@users.noreply.github.com>
2022-10-02 11:27:18 +0900

    本番環境の修正v2.1

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3002" || "https://fit-wearness-front.herokuapp.com"
+    # origins "localhost:3002" || "https://fit-wearness-front.herokuapp.com"
+    origins "https://fit-wearness-front.herokuapp.com"
     # ReactのポートとヘロクのURL
 
     resource '*',

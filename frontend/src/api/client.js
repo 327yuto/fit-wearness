69b73361427f306db2dfbba037f31e2bf4b4570d
327yuto <78023458+327yuto@users.noreply.github.com>
2022-10-05 18:57:00 +0900

    本番環境の修正v3.1

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -12,7 +12,7 @@ const options = {
 
 const client = applyCaseMiddleware(axios.create({
   // baseURL: "http://localhost:3020/api/v1"
-  baseURL: "https://fit-wearness.herokuapp.com/api/v1"
+  baseURL: "https://fit-wearness.herokuapp.com/api/v1",
 }), options)
 
 export default client

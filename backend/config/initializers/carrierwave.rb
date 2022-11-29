51a09d3d2a6969ab46c845f86df7ce0dbefed21f
327yuto <78023458+327yuto@users.noreply.github.com>
2022-11-29 22:50:22 +0900

    画像投稿機能　本場環境をS3に設定

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -5,16 +5,16 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
 
   if Rails.env.production?
-    # config.storage :fog
-    # config.fog_provider = 'fog/aws'
-    # config.fog_directory  = 'バケット名' # 作成したバケット名を記述
-    # config.fog_credentials = {
-    #   provider: 'AWS',
-    #   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-    #   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-    #   region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
-    #   path_style: true
-    # }
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'fit-wearness-app' # 作成したバケット名を記述
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+      path_style: true
+    }
   else
     config.asset_host = 'http://localhost:3020'
     config.storage = :file

6d12ccef471b31ee162777996b887efb2791d2c4
327yuto <78023458+327yuto@users.noreply.github.com>
2022-11-29 23:24:43 +0900

    S3　keyの記入を変更

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -10,8 +10,13 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'fit-wearness-app' # 作成したバケット名を記述
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],   # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],   # 環境変数
+      
+      # aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],   # 環境変数
+      aws_access_key_id: 'AKIA2F7A5FFQ2AL6FFSL'
+      
+      # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],   # 環境変数
+      aws_secret_access_key: '2y28N8uPa4DOqTzujZbC6Bc3LJyy3S03ZTgxxjuq'
+
       region: 'ap-northeast-1',    # アジアパシフィック(東京)を選択した場合
       path_style: true
     }

63cdd516ff3a45ef3e420d567a6e5c750b006228
327yuto <78023458+327yuto@users.noreply.github.com>
2022-12-10 16:15:21 +0900

    Preflight requestの設定を追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+ 
+
   namespace :api do
     namespace :v1 do
       resources :test, only: %i[index]
@@ -10,7 +12,6 @@ Rails.application.routes.draw do
 
       get '/my_liked_posts', to: 'posts#my_liked_posts', as: :my_liked_posts
 
-
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
@@ -19,6 +20,9 @@ Rails.application.routes.draw do
         resources :sessions, only: %i[index]
       end
       
+      match '*path' => 'options_request#response_preflight_request', via: :options
+
+
     end
   end
 end

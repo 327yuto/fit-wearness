40e0dadec139ead131dd4f62fc931bef3df443ed
327yuto <78023458+327yuto@users.noreply.github.com>
2023-01-01 21:25:59 +0900

    AWS　DB接続の修正

--- backend/start.sh
+++ backend/start.sh
@@ -2,8 +2,8 @@
 
 if [ "${RAILS_ENV}" = "production" ]
 then
-   bundle exec rails assets:precompile
-   bundle exec rails db:create && bundle exec rails db:migrate && bundle exec unicorn -p 3000 -c /backend/config/unicorn.rb -E production
+   # bundle exec rails assets:precompile
+   /bin/sh -c bundle exec rails db:create && bundle exec rails db:migrate && bundle exec unicorn -p 3000 -c /backend/config/unicorn.rb -E production
 else
 bundle exec rails s -p ${PORT:-3020} -b 0.0.0.0
 fi
